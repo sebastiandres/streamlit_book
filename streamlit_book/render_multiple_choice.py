366e62eb8e9cf5c4a731be96dcbf2ea126d169cd
sebastiandres <sebastiandres@gmail.com>
2022-02-23 17:02:02 -0300

    Allowing activities to be rendered in isolation

--- streamlit_book/render_multiple_choice.py
+++ streamlit_book/render_multiple_choice.py
@@ -11,6 +11,66 @@ except:
     from .keywords import BUTTON, SUCCESS, ERROR, DEFAULT_SUCCESS_MESSAGE, DEFAULT_ERROR_MESSAGE, DEFAULT_BUTTON_MESSAGE
     from .answers import save_answer
 
+
+def multiple_choice(question, options_dict,  
+                    success=DEFAULT_SUCCESS_MESSAGE, 
+                    error=DEFAULT_ERROR_MESSAGE, 
+                    button=DEFAULT_BUTTON_MESSAGE):
+    """Render a multiple choice question from the given parameters.
+
+    :param question: question to be displayed before the multiple-choice options
+    :type question: str
+    :param options_dict: dictionary of options to be displayed, with the option text as key and the boolean answer as value
+    :type options: dict
+    :param answer: index (starting at 0) of the expected answer to the question
+    :type answer: int
+    :param success: message to be displayed when the user answers correctly
+    :type success: str, optional
+    :param error: message to be displayed when the user answers incorrectly
+    :type error: str, optional
+    :param button: message to be displayed on the button that checks the answer
+    :type button: str, optional
+    :return: tuple of booleans with button press status and correctness of answer
+    :rtype: tuple of bool
+    """
+    cb_list = []
+    if len(question)==0:
+        st.error("Please provide a question")
+        return None, None
+    elif len(options_dict)<2:
+        st.error("Must have at least 2 options")
+        return None, None
+    else:
+        # Write the question
+        st.markdown(question)
+        # Write the options and append the user answers into the list cb_list
+        for option, answer in options_dict.items():
+            key = (question + option + str(answer)).lower().replace(" ", "_")
+            # Post the option with no option
+            cb = st.checkbox(option, value=False, key=key)
+            cb_list.append(cb)
+        # Check if the correct checkboxes are checked
+        key = ("multiple-choice:" + question + "".join(options_dict.keys())).lower().replace(" ", "_")
+        if st.button(button, key=key):
+            # Check answers
+            if all(u==a for u, a in zip(cb_list, options_dict.values())):
+                st.success(success)
+                is_correct = True
+            else:
+                st.error(error)
+                is_correct = False
+            # Save the answers, if required
+            if "save_answers" in st.session_state and st.session_state.save_answers:                
+                user_answer = [list(options_dict.keys())[i] for i, cb in enumerate(cb_list) if cb]
+                correct_answer = [key for key, val in options_dict.items() if val]
+                save_answer(question, is_correct=is_correct, user_answer=user_answer, correct_answer=correct_answer)
+            # Return if button is pressed and answer evaluation
+            return True, is_correct
+        else:
+            # Return if button is not pressed
+            return False, None
+
+
 def multiple_choice_parser(lines):
     """Parses a list of lines into a dictionary with the parsed values.
 
@@ -49,60 +109,6 @@ def multiple_choice_parser(lines):
             parse_dict["question"] += line + "\n"
     return parse_dict
 
-
-def multiple_choice(question, options_dict,  
-                    success=DEFAULT_SUCCESS_MESSAGE, 
-                    error=DEFAULT_ERROR_MESSAGE, 
-                    button=DEFAULT_BUTTON_MESSAGE):
-    """Render a multiple choice question from the given parameters.
-
-    :param question: question to be displayed before the multiple-choice options
-    :type question: str
-    :param options_dict: dictionary of options to be displayed, with the option text as key and the boolean answer as value
-    :type options: dict
-    :param answer: index (starting at 0) of the expected answer to the question
-    :type answer: int
-    :param success: message to be displayed when the user answers correctly
-    :type success: str, optional
-    :param error: message to be displayed when the user answers incorrectly
-    :type error: str, optional
-    :param button: message to be displayed on the button that checks the answer
-    :type button: str, optional
-    :return: boolean with the exit status of the function
-    :rtype: bool
-    """
-    cb_list = []
-    if len(question)==0:
-        st.error("Please provide a question")
-        return False
-    elif len(options_dict)<2:
-        st.error("Must have at least 2 options")
-        return False
-    else:
-        # Write the question
-        st.markdown(question)
-        # Write the options and append the user answers into the list cb_list
-        for option, answer in options_dict.items():
-            key = (question + option + str(answer)).lower().replace(" ", "_")
-            # Post the option with no option
-            cb = st.checkbox(option, value=False, key=key)
-            cb_list.append(cb)
-        # Check if the correct checkboxes are checked
-        key = ("multiple-choice:" + question + "".join(options_dict.keys())).lower().replace(" ", "_")
-        if st.button(button, key=key):
-            # Check answers
-            if all(u==a for u, a in zip(cb_list, options_dict.values())):
-                st.success(success)
-                is_correct = True
-            else:
-                st.error(error)
-                is_correct = False
-            # Save the answers, if required
-            if st.session_state.save_answers:                
-                user_answer = [list(options_dict.keys())[i] for i, cb in enumerate(cb_list) if cb]
-                correct_answer = [key for key, val in options_dict.items() if val]
-                save_answer(question, is_correct=is_correct, user_answer=user_answer, correct_answer=correct_answer)
-        return True
 
 def multiple_choice_from_lines(lines):
     """Renders a multiple choice question from a list of lines.
--- streamlit_book/render_single_choice.py
+++ streamlit_book/render_single_choice.py
@@ -11,6 +11,56 @@ except:
     from .keywords import BUTTON, SUCCESS, ERROR, DEFAULT_SUCCESS_MESSAGE, DEFAULT_ERROR_MESSAGE, DEFAULT_BUTTON_MESSAGE
     from .answers import save_answer
 
+
+def single_choice(question, options, answer_index,
+                    success=DEFAULT_SUCCESS_MESSAGE, 
+                    error=DEFAULT_ERROR_MESSAGE, 
+                    button=DEFAULT_BUTTON_MESSAGE):
+    """Renders a single-choice question from arguments.
+
+    :param question: question to be displayed before the single-choice options
+    :type question: str
+    :param options: list of options to be displayed.
+    :type options: str
+    :param answer: index (starting at 0) of the expected answer to the question
+    :type answer: int
+    :param success: message to be displayed when the user answers correctly
+    :type success: str, optional
+    :param error: message to be displayed when the user answers incorrectly
+    :type error: str, optional
+    :param button: message to be displayed on the button that checks the answer
+    :type button: str, optional
+    :return: tuple of booleans with button press status and correctness of answer
+    :rtype: tuple of bool
+    """
+    if len(question)==0:
+        st.error("Please provide a question")
+        return None, None
+    elif len(options)<2:
+        st.error("Must have at least 2 options")
+        return None, None
+    else:
+        # Write the options with radio button: only one option can be selected
+        user_answer = st.radio(question, options=options)
+        # Check if correct answer
+        key = ("single-choice:" + question + "".join(options)).lower().replace(" ", "_")
+        if st.button(button, key=key):
+            # Check ansers
+            if options.index(user_answer) == answer_index:
+                st.success(success)
+                is_correct = True
+            else:
+                st.error(error)
+                is_correct = False
+            # Save the answers, if required
+            if "save_answers" in st.session_state and st.session_state.save_answers:
+                save_answer(question, is_correct=is_correct, user_answer=user_answer, correct_answer=options[answer_index])
+            # Return if button is pressed and answer evaluation
+            return True, is_correct
+        else:
+            # Return if button is not pressed
+            return False, None
+
 def single_choice_parser(lines):
     """Parses a list of lines into a dictionary with the parsed values.
 
@@ -54,50 +104,6 @@ def single_choice_parser(lines):
     parse_dict["answer_index"] = parse_dict["options"].index(answer)
     return parse_dict
 
-def single_choice(question, options, answer_index,
-                    success=DEFAULT_SUCCESS_MESSAGE, 
-                    error=DEFAULT_ERROR_MESSAGE, 
-                    button=DEFAULT_BUTTON_MESSAGE):
-    """Renders a single-choice question from arguments.
-
-    :param question: question to be displayed before the single-choice options
-    :type question: str
-    :param options: list of options to be displayed.
-    :type options: str
-    :param answer: index (starting at 0) of the expected answer to the question
-    :type answer: int
-    :param success: message to be displayed when the user answers correctly
-    :type success: str, optional
-    :param error: message to be displayed when the user answers incorrectly
-    :type error: str, optional
-    :param button: message to be displayed on the button that checks the answer
-    :type button: str, optional
-    :return: boolean with the exit status of the function
-    :rtype: bool
-    """
-    if len(question)==0:
-        st.error("Please provide a question")
-        return False
-    elif len(options)<2:
-        st.error("Must have at least 2 options")
-        return False
-    else:
-        # Write the options with radio button: only one option can be selected
-        user_answer = st.radio(question, options=options)
-        # Check if correct answer
-        key = ("single-choice:" + question + "".join(options)).lower().replace(" ", "_")
-        if st.button(button, key=key):
-            # Check ansers
-            if options.index(user_answer) == answer_index:
-                st.success(success)
-                is_correct = True
-            else:
-                st.error(error)
-                is_correct = False
-        # Save the answers, if required
-            if st.session_state.save_answers:
-                save_answer(question, is_correct=is_correct, user_answer=user_answer, correct_answer=options[answer_index])
-        return True
 
 
 def single_choice_from_lines(lines):
